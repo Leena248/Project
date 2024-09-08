2aab2a970003964db88cd1f73ae306f2c9c31939
Leena <leenaheshamm@gmail.com>
2024-09-08 23:55:42 +0300

    Money transfer page with 3 different components. one for each tranfer status

--- Project/src/app/money-transfer/money-transfer.component.ts
+++ Project/src/app/money-transfer/money-transfer.component.ts
@@ -4,16 +4,20 @@ import { NavbarComponent } from '../components/navbar/navbar.component';
 import { MobileSectionComponent } from '../components/mobile-section/mobile-section.component';
 import { FooterComponent } from '../components/footer/footer.component';
 import { ConfirmationComponent } from '../MoneyTransferComp/confirmation/confirmation.component';
+import { MoneyTransferIntroSectionComponent } from '../MoneyTransferComp/money-transfer-intro-section/money-transfer-intro-section.component';
+import { SuccessComponent } from '../MoneyTransferComp/success/success.component';
 
 @Component({
   selector: 'app-money-transfer',
   standalone: true,
   imports: [
     AmountComponent,
+    SuccessComponent,
     NavbarComponent,
     MobileSectionComponent,
     FooterComponent,
     ConfirmationComponent,
+    MoneyTransferIntroSectionComponent,
   ],
   templateUrl: './money-transfer.component.html',
   styleUrl: './money-transfer.component.scss',
